f44aa3c5671bb1c5b6847fba253059aff635017f
Nicoreganono <nico.fou@live.ca>
2026-09-15 16:36:42 -0400

    Imported originally made p5.js code for miami landscape

--- topics/instructions/landscape-challenge/js/script.js
+++ topics/instructions/landscape-challenge/js/script.js
@@ -1,24 +1,61 @@
 /**
- * Title of Project
- * Author Name
+ * Hotline Miami
+ * Nicola, Alice
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A brute forced representation of a late evening Miami Florida landscape that we both worked very hard for.
  */
 
 "use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+  createCanvas(900, 700);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+  noStroke()
+  background(220, 60, 80);
+  fill(230,70,80)
+  rect(0,160,900,700)
+  fill(240,80,80)
+  rect(0,240,900,700)
+  fill(250,100,80)
+  rect(0,300,900,700)
+  fill(255,110,80)
+  rect(0,340,900,700)
+  fill(255,130,80)
+    rect(0,370,900,700)
+  fill(255,150,80)
+    rect(0,395,900,700)
+  fill(255,160,80)
+    rect(0,415,900,700)
+  fill(255,170,80)
+    rect(0,430,900,700)
+  fill(255,180,80)
+    rect(0,440,900,700)
+  fill(255,190,80)
+    rect(0,445,900,700)
+  fill(255,255,255)
+  strokeWeight(20)
+  stroke("#ffbe50")
+  ellipse(200, 450, 150)
+  noStroke()
+  fill(0,0,0)
+  rect(0, 540, 900, 700)
+  rect(800, 400, 50, 200)
+  rect(850, 450, 50, 100)
+  rect(750, 450, 50, 100)
+  rect(650, 450, 50, 100)
+  rect(550, 450, 50, 100)
+  rect(450, 450, 50, 100)
+  rect(350, 450, 50, 100)
+  rect(250, 450, 50, 100)
+    rect(150, 450, 50, 100)
+    rect(50, 450, 50, 100)
+  rect(700, 400, 50, 200)
+    rect(600, 400, 50, 200)
+    rect(500, 400, 50, 200)
+    rect(400, 400, 50, 200)
+    rect(300, 400, 50, 200)
+    rect(200, 400, 50, 200)
+    rect(100, 400, 50, 200)
+    rect(0, 400, 50, 200) 
 }
