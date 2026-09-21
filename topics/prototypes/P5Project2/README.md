feaf0bfab262650adb84def56cc6e58f92cf5649
Nicoreganono <nico.fou@live.ca>
2026-09-21 19:54:45 -0400

    Added template file for second Version of prototyping

---
+++ topics/prototypes/P5Project2/README.md
@@ -0,0 +1,21 @@
+# Void Eater
+
+Nicola Fournier
+
+[View this project online](https://nicoreganono.github.io/cart253/topics/prototypes/P5Project1/)
+
+## Description
+
+This arrangement of p5.js represents a large mouth with large teeth with two eyes inside.
+
+## Attribution
+
+This bit should attribute any code, assets or other elements used taken from other sources. For example:
+
+> - This project uses [p5.js](https://p5js.org).
+> - The clown image is a capture of the clown from the Apple emoji character set.
+> - The barking sound effect is "single dog bark 1" by crazymonke9 from freesound.org: https://freesound.org/people/crazymonke9/sounds/418107/
+
+## License
+
+No license required!
---
+++ topics/prototypes/P5Project2/js/script.js
@@ -0,0 +1,57 @@
+/**
+ * Voideater
+ * Nicola Fournier
+ * 
+ * This is project 1 of 3 of p5 script manipulation.
+ */
+
+"use strict";
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    //Dark background
+    background(30, 20, 50);
+    //Make a sharp lower teeth row
+    fill("white")
+    triangle(-100, 400, 130, 340, 200, 400)
+    triangle(0, 400, 70, 310, 80, 400)
+    triangle(50, 400, 85, 300, 140, 400)
+    triangle(-100, 400, 20, 300, 50, 400)
+    triangle(500, 400, 270, 340, 200, 400)
+    triangle(400, 400, 330, 310, 320, 400)
+    triangle(350, 400, 315, 300, 260, 400)
+    triangle(500, 400, 380, 300, 350, 400)
+    //Make upper teeths roots appear
+    triangle(40, 80, 170, 30, 200, 55)
+    triangle(30, 80, 80, 30, 100, 65)
+    triangle(-50, 100, 50, 30, 70, 70)
+    triangle(70, 70, 100, 30, 150, 60)
+    triangle(360, 80, 230, 30, 200, 55)
+    triangle(370, 80, 320, 30, 300, 65)
+    triangle(450, 100, 350, 30, 330, 70)
+    triangle(330, 70, 300, 30, 250, 60)
+    //Make an upper teeth row
+    triangle(40, 80, 170, 140, 200, 55)
+    triangle(30, 80, 80, 160, 100, 65)
+    triangle(-50, 100, 50, 180, 70, 70)
+    triangle(70, 70, 100, 180, 150, 60)
+    triangle(360, 80, 230, 140, 200, 55)
+    triangle(370, 80, 320, 160, 300, 65)
+    triangle(450, 100, 350, 180, 330, 70)
+    triangle(330, 70, 300, 180, 250, 60)
+    //Eyes
+    ellipse(130, 230, 100)
+    ellipse(270, 230, 100)
+
+    fill("black")
+    ellipse(130, 230, 30)
+
+    ellipse(270, 230, 50)
+    fill("white")
+    ellipse(280, 222, 15)
+    ellipse(135, 225, 10)
+    //Now he looks like caine from TADC, but oh well, I think its cool
+}
