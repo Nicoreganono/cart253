aaf2620a18547c08bdefc9bb9e1f447a61fe3731
Nicoreganono <nico.fou@live.ca>
2026-09-21 19:44:14 -0400

    Added upper teeth and their roots

--- topics/prototypes/P5Project1/template-p5-project/js/script.js
+++ topics/prototypes/P5Project1/template-p5-project/js/script.js
@@ -24,5 +24,22 @@ function draw() {
     triangle(400, 400, 330, 310, 320, 400)
     triangle(350, 400, 315, 300, 260, 400)
     triangle(500, 400, 380, 300, 350, 400)
-
+    //Make upper teeths roots appear
+    triangle(40, 80, 170, 30, 200, 55)
+    triangle(30, 80, 80, 30, 100, 65)
+    triangle(-50, 100, 50, 30, 70, 70)
+    triangle(70, 70, 100, 30, 150, 60)
+    triangle(360, 80, 230, 30, 200, 55)
+    triangle(370, 80, 320, 30, 300, 65)
+    triangle(450, 100, 350, 30, 330, 70)
+    triangle(330, 70, 300, 30, 250, 60)
+    //Make an upper teeth row
+    triangle(40, 80, 170, 140, 200, 55)
+    triangle(30, 80, 80, 160, 100, 65)
+    triangle(-50, 100, 50, 180, 70, 70)
+    triangle(70, 70, 100, 180, 150, 60)
+    triangle(360, 80, 230, 140, 200, 55)
+    triangle(370, 80, 320, 160, 300, 65)
+    triangle(450, 100, 350, 180, 330, 70)
+    triangle(330, 70, 300, 180, 250, 60)
 }
