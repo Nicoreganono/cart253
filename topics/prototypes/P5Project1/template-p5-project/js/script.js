e70b59e49c152aa44dbe813c56fb79832911e7dd
Nicoreganono <nico.fou@live.ca>
2026-09-21 15:34:27 -0400

    Added first visual feature + Title project and author
    first visual feature is a lower row of teeth.

--- topics/prototypes/P5Project1/template-p5-project/js/script.js
+++ topics/prototypes/P5Project1/template-p5-project/js/script.js
@@ -1,24 +1,28 @@
 /**
- * Title of Project
- * Author Name
+ * Voideater
+ * Nicola Fournier
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This is project 1 of 3 of p5 script manipulation.
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+    createCanvas(400, 400);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    //Dark background
+    background(30, 20, 50);
+    //Make a sharp lower teeth row
+    fill("white")
+    triangle(-100, 400, 130, 340, 200, 400)
+    triangle(0, 400, 70, 310, 80, 400)
+    triangle(50, 400, 85, 300, 140, 400)
+    triangle(-100, 400, 20, 300, 50, 400)
+    triangle(500, 400, 270, 340, 200, 400)
+    triangle(400, 400, 330, 310, 320, 400)
+    triangle(350, 400, 315, 300, 260, 400)
+    triangle(500, 400, 380, 300, 350, 400)
 
 }
