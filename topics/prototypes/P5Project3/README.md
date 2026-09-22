2ac26e86e7da12737a12dd96380af132b2358a6c
Nicoreganono <nico.fou@live.ca>
2026-09-22 11:41:36 -0400

    Created P5Project3 and started it's .js with a bunch of ellipses

---
+++ topics/prototypes/P5Project3/README.md
@@ -0,0 +1,21 @@
+# LazerTank
+
+Nicola Fournier
+
+[View this project online](https://nicoreganono.github.io/cart253/topics/prototypes/P5Project2/)
+
+## Description
+
+This arrangement of p5.js represents a small blocky tank shooting out a devastating beam of light.
+
+## Attribution
+
+This bit should attribute any code, assets or other elements used taken from other sources. For example:
+
+> - This project uses [p5.js](https://p5js.org).
+> - The clown image is a capture of the clown from the Apple emoji character set.
+> - The barking sound effect is "single dog bark 1" by crazymonke9 from freesound.org: https://freesound.org/people/crazymonke9/sounds/418107/
+
+## License
+
+No license required!
---
+++ topics/prototypes/P5Project3/js/script.js
@@ -0,0 +1,67 @@
+/**
+ * OrbitConflict
+ * Nicola Fournier
+ * 
+ * This is project 3 of 3 of p5 script manipulation.
+ */
+
+"use strict";
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+
+  //Make rings out of a whole bunch of ellipses
+     ellipse(180,120,20)
+    ellipse(150,125,25)
+    ellipse(120,132,30)
+    ellipse(100,140,42)
+    ellipse(85,150,56)
+  ellipse(75,165,70)
+  ellipse(80,180,84)
+  ellipse(95,188,92)
+  ellipse(115,194,100)
+  ellipse(145,200,104)
+  ellipse(180,204,108)
+  
+      ellipse(220,120,20)
+    ellipse(250,125,25)
+    ellipse(280,132,30)
+    ellipse(300,140,42)
+    ellipse(315,150,56)
+  ellipse(325,165,70)
+  ellipse(320,180,84)
+  ellipse(305,188,92)
+  ellipse(285,194,100)
+  ellipse(255,200,104)
+  ellipse(220,204,108)
+
+      ellipse(200,180,20)
+    ellipse(200,150,25)
+    ellipse(200,120,30)
+    ellipse(200,100,42)
+    ellipse(200,85,56)
+  ellipse(200,75,70)
+  ellipse(200,80,84)
+  ellipse(200,95,92)
+  ellipse(200,115,100)
+  ellipse(200,145,104)
+  ellipse(200,180,108)
+  
+      ellipse(200,220,20)
+    ellipse(200,250,25)
+    ellipse(200,280,30)
+    ellipse(200,300,42)
+    ellipse(200,315,56)
+  ellipse(200,325,70)
+  ellipse(200,320,84)
+  ellipse(200,305,92)
+  ellipse(200,285,100)
+  ellipse(200,255,104)
+  ellipse(200,220,108)
+    
+  
+ 
+}
