b93f7c04f4a991ea2be8eaf757de8cfc61054002
Nicoreganono <nico.fou@live.ca>
2026-09-22 12:18:28 -0400

    Painfully made all of them rotate at different rates

--- topics/prototypes/P5Project3/js/script.js
+++ topics/prototypes/P5Project3/js/script.js
@@ -14,54 +14,150 @@ function setup() {
 function draw() {
 
   //Make rings out of a whole bunch of ellipses
-     ellipse(180,120,20)
+  //For some foolish reason, spend hours of time to make them rotate
+  //I hate it
+  //I hated that
+  //Looks cool!
+  //NOT WORTH
+ let angle = frameCount * 0.001;
+  rotate(angle)
+    ellipse(180,120,20)
+      let angle1 = frameCount * -0.002;
+  rotate(angle1)
     ellipse(150,125,25)
+      let angle2 = frameCount * 0.003;
+  rotate(angle2)
     ellipse(120,132,30)
+      let angle3 = frameCount * -0.004;
+  rotate(angle3)
     ellipse(100,140,42)
+      let angle4 = frameCount * 0.005;
+  rotate(angle4)
     ellipse(85,150,56)
+      let angle5 = frameCount * -0.006;
+  rotate(angle5)
   ellipse(75,165,70)
+      let angle6 = frameCount * 0.007;
+  rotate(angle6)
   ellipse(80,180,84)
+      let angle7 = frameCount * -0.008;
+  rotate(angle7)
   ellipse(95,188,92)
+      let angle8 = frameCount * 0.009;
+  rotate(angle8)
   ellipse(115,194,100)
+      let angle9 = frameCount * -0.01;
+  rotate(angle9)
   ellipse(145,200,104)
+      let angle10 = frameCount * 0.011;
+  rotate(angle10)
   ellipse(180,204,108)
   
+  let angle0 = frameCount * -0.012;
+  rotate(angle0)
+  ellipse(180,204,108)
+  let angle11 = frameCount * 0.013;
+  rotate(angle11)
       ellipse(220,120,20)
+  let angle12 = frameCount * -0.014;
+  rotate(angle12)
+  let angle13 = frameCount * 0.015;
+  rotate(angle13)
     ellipse(250,125,25)
+  let angle14 = frameCount * -0.016;
+  rotate(angle14)
     ellipse(280,132,30)
+  let angle15 = frameCount * 0.017;
+  rotate(angle15)
     ellipse(300,140,42)
+  let angle16 = frameCount * -0.018;
+  rotate(angle16)
     ellipse(315,150,56)
+  let angle17 = frameCount * 0.019;
+  rotate(angle17)
   ellipse(325,165,70)
+  let angle18 = frameCount * -0.02;
+  rotate(angle18)
   ellipse(320,180,84)
+  let angle19 = frameCount * 0.021;
+  rotate(angle19)
   ellipse(305,188,92)
+  let angle20 = frameCount * -0.022;
+  rotate(angle20)
   ellipse(285,194,100)
+  let angle21 = frameCount * 0.023;
+  rotate(angle21)
   ellipse(255,200,104)
+  let angle22 = frameCount * -0.024;
+  rotate(angle22)
   ellipse(220,204,108)
-
+  let angle23 = frameCount * 0.025;
+  rotate(angle23)
       ellipse(200,180,20)
+  let angle24 = frameCount * -0.026;
+  rotate(angle24)
     ellipse(200,150,25)
+  let angle25 = frameCount * 0.027;
+  rotate(angle25)
     ellipse(200,120,30)
+  let angle26 = frameCount * -0.028;
+  rotate(angle26)
     ellipse(200,100,42)
+  let angle27 = frameCount * 0.029;
+  rotate(angle27)
     ellipse(200,85,56)
+  let angle28 = frameCount * -0.03;
+  rotate(angle28)
   ellipse(200,75,70)
+  let angle29 = frameCount * 0.031;
+  rotate(angle29)
   ellipse(200,80,84)
+  let angle30 = frameCount * -0.032;
+  rotate(angle30)
   ellipse(200,95,92)
+  let angle31 = frameCount * 0.033;
+  rotate(angle31)
   ellipse(200,115,100)
+  let angle32 = frameCount * -0.034;
+  rotate(angle32)
   ellipse(200,145,104)
+  let angle33 = frameCount * 0.035;
+  rotate(angle33)
   ellipse(200,180,108)
-  
+
+  let angle34 = frameCount * -0.036;
+  rotate(angle34)
       ellipse(200,220,20)
+  let angle35 = frameCount * 0.037;
+  rotate(angle35)
     ellipse(200,250,25)
+  let angle36 = frameCount * -0.038;
+  rotate(angle36)
     ellipse(200,280,30)
+  let angle37 = frameCount * 0.039;
+  rotate(angle37)
     ellipse(200,300,42)
+  let angle38 = frameCount * -0.04;
+  rotate(angle38)
     ellipse(200,315,56)
+  let angle39 = frameCount * 0.041;
+  rotate(angle39)
   ellipse(200,325,70)
+  let angle40 = frameCount * -0.042;
+  rotate(angle40)
   ellipse(200,320,84)
+    let angle41 = frameCount * 0.043;
+  rotate(angle41)
   ellipse(200,305,92)
+  let angle42 = frameCount * -0.044;
+  rotate(angle42)
   ellipse(200,285,100)
+  let angle43 = frameCount * 0.045;
+  rotate(angle43)
   ellipse(200,255,104)
+  let angle44 = frameCount * -0.046;
+  rotate(angle44)
   ellipse(200,220,108)
-    
-  
  
+ //Now it's a bunch of conflicting orbits
 }
