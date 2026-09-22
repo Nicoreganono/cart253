df56dd1425428e0124215620f156303d1e9bc448
Nicoreganono <nico.fou@live.ca>
2026-09-22 15:51:21 -0400

    Started with making mr roid turn red

--- topics/instructions/variables-challenge/js/script.js
+++ topics/instructions/variables-challenge/js/script.js
@@ -1,24 +1,48 @@
 /**
- * Title of Project
- * Author Name
+ * RoidRage
+ * Nicola Fournier
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * He is VERY ANGRY and the birds make him INCREDIBLY ANGRY
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
+};
+
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
-
+  background(160, 180, 200);
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  mrFurious.fill.r = mrFurious.fill.r + 1
+mrFurious.fill.g = mrFurious.fill.g - 0.5
+mrFurious.fill.b = mrFurious.fill.b - 0.5
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
 }
